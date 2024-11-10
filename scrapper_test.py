6077f256c668a00ac0cbf3e6d1ca8e82c1a34d35
James <jahsoftwaredev@outlook.com>
2024-11-09 16:16:17 -0800

    able to seperate data into rows

--- scrapper_test.py
+++ scrapper_test.py
@@ -6,6 +6,11 @@ url = "https://www.pro-football-reference.com/teams/buf/2024.htm"
 web_page = requests.get(url)
 doc = BeautifulSoup(web_page.text, "html.parser")
 
-tags = doc.find_all(["tbody"])[4] #the table where the data is located 
+tbody = doc.find_all(["tbody"])[4] #the table where the data is located 
+# print(tbody) # delete first hashtag to check the data table is captured
 
-print(tags)
+trs = tbody.contents #the table rows of each player
+# print(trs) # delete first hashtag to check the rows are captured
+
+for tr in trs:
+    print(tr)
