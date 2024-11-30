0369191db9fe75b96695fca23568534cc19e8f19
James <jahsoftwaredev@outlook.com>
2024-11-29 18:59:57 -0800

    almost done with filtering players from one team

--- scrapper_test.py
+++ scrapper_test.py
@@ -1,3 +1,4 @@
+from qb_obj import QB_Player
 from bs4 import BeautifulSoup
 import requests 
 
@@ -6,26 +7,29 @@ url = "https://www.pro-football-reference.com/teams/buf/2024.htm"
 web_page = requests.get(url)
 doc = BeautifulSoup(web_page.text, "html.parser")
 
+## BUFFALO BILLS
+buffalo_bills_players = []
+
+# QB TABLE STATS FOR PASSING
+
+QB_table = doc.find_all(["tbody"])[3]
+QB_trs = QB_table.find_all("tr")
+
+# col 0 = name, col 3 = games played, col 6 = completions, col 7 = attempts, col 9 = yards
+# col 10 = tds, col 12 = interceptions
+for row in QB_trs:
+    cols = row.find_all('td')
+    if int(cols[7].text.strip()) > 50: # this is a filter to exclude QBs that didnt play very much
+        QB = QB_Player(cols[0].text.strip(), "QB", cols[3].text.strip(), cols[6].text.strip(), cols[7].text.strip(), cols[9].text.strip(), cols[10].text.strip(), cols[12].text.strip())
+        buffalo_bills_players.append(QB);
+
+# THE TABLE FOR RUSHING AND RECEIVING STATS
+
 table = doc.find_all(["tbody"])[4] #the table where the data is located 
-# print(tbody) # delete first hashtag to check the data table is captured
-
-#trs = tbody.contents #the table rows of each player
-# print(trs) # delete first hashtag to check the rows are captured
-
-#for tr in trs:
- #   print(tr)
-
-#print(table.find_all(["tr", "data-stat"]))
-
 trs = table.find_all("tr")
 
+#col 0 = name, col 2 = position, col 3 = games played, col 5 = rush attempts, 
+# col 6 = rush yards, col 7 = rush TD, col 14 = rec tgts, col 15 = rec yards, col 17 = rec TDs
 for row in trs:
     cols = row.find_all("td")
-
-    if cols:
-        for td in cols:
-            #print(td.text)
-            data_text = td.text.strip() if td.text.strip() else 0
-            print(data_text)
-
-        print()
+     
