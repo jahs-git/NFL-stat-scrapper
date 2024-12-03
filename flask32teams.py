d1bd1209b6c8b9e83284d862d42abe9ae70451a7
Shoua <128767893+shaymua@users.noreply.github.com>
2024-12-02 21:41:07 -0800

    Update flask32teams.py

--- flask32teams.py
+++ flask32teams.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Pass the nfl_standings dictionary to the template
-    return render_template('index.html', nfl_standings=nfl_standings)
+    return render_template('Team.Grid.html', nfl_standings=nfl_standings)
 
 if __name__ == "__main__":
     app.run(debug=True)
