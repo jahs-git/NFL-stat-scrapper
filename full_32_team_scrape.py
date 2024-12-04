ed63ef6b5dcb6a94561bf19e877cac87d795c39a
James <jahsoftwaredev@outlook.com>
2024-12-03 17:15:38 -0800

    no changes done, in progress of restructing datat for front end

--- full_32_team_scrape.py
+++ full_32_team_scrape.py
@@ -51,16 +51,17 @@ for team in teams:
     web_page = requests.get(url)
     doc = BeautifulSoup(web_page.text, "html.parser")
     team_players = []
-    # QB TABLE STATS FOR PASSING
+    #QB TABLE STATS FOR PASSING
+    print(team[0])
     QB_table = doc.find_all(["tbody"])[3]
     QB_trs = QB_table.find_all("tr")
     # col 0 = name, col 3 = games played, col 6 = completions, col 7 = attempts, col 9 = yards
     # col 10 = tds, col 12 = interceptions
     for row in QB_trs:
-        cols = row.find_all('td')
-        if int(cols[7].text.strip()) > 50: # this is a filter to exclude QBs that didnt play very much
-             QB = QB_Player(cols[0].text.strip(), "QB", cols[3].text.strip(), cols[6].text.strip(), cols[7].text.strip(), cols[9].text.strip(), cols[10].text.strip(), cols[12].text.strip())
-             team[2].append(QB);
+       cols = row.find_all('td')
+       if int(cols[7].text.strip()) > 50: # this is a filter to exclude QBs that didnt play very much
+            QB = QB_Player(cols[0].text.strip(), "QB", cols[3].text.strip(), cols[6].text.strip(), cols[7].text.strip(), cols[9].text.strip(), cols[10].text.strip(), cols[12].text.strip())
+            team[2].append(QB);
 
 # THE TABLE FOR RUSHING AND RECEIVING STATS
 
@@ -78,10 +79,14 @@ for team in teams:
                     player.rushing_attempt = cols[5].text.strip()
                     player.rush_yards = cols[6].text.strip()
                     player.rush_td = cols[7].text.strip()
-                    break
+                    
         elif (int(cols[15].text.strip()) >= 10 or int(cols[5].text.strip()) > 25):
             player = Player(cols[0].text.strip(), cols[2].text.strip(), cols[3].text.strip(), cols[5].text.strip(), cols[6].text.strip(), cols[7].text.strip(), cols[14].text.strip(), cols[15].text.strip(), cols[16].text.strip(), cols[18].text.strip()) 
             team[2].append(player)
+
+
+
+player_list = []
 
 for team_name, team_link, players in teams:
     print("====================")
@@ -89,7 +94,19 @@ for team_name, team_link, players in teams:
     print("====================")
 
     for playa in players:
+        #player_list.append((player.name(), team_name()))
         playa.print_stats()
+#for player_name, team_name in player_list:
+    #print(player_name, team_name)
 
+#def full_team_stats(user_team_name):
+    #for team_name, team_link, players in teams:
+        #if(team_name.lower() == user_team_name):
+           #for player in team:
+                #player.print_stats()
+
+
+#user_input = input("enter team name to display stats: ")
+#full_team_stats(user_input.lower())
 
 
